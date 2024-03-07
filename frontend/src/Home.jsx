63d347d1cbe998dfeb702f7ce9f49101d92711c2
Rajesh Adeli <rajannaadeli@gmail.com>
2024-03-07 22:40:55 +0530

    Changing some small configurations

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -19,9 +19,9 @@ const Home = () => {
             key,
             amount: order.amount,
             currency: "INR",
-            name: "6 Pack Programmer",
-            description: "Tutorial of RazorPay",
-            image: "https://avatars.githubusercontent.com/u/25058652?v=4",
+            name: "Holi Event Payment.",
+            description: "Paying to this will give you the tickets for the Event",
+            image: "https://avatars.githubusercontent.com/u/114233607?s=96&v=4",
             order_id: order.id,
             callback_url: "http://localhost:4000/api/paymentverification",
             prefill: {
@@ -42,12 +42,9 @@ const Home = () => {
 
     return (
         <Box>
-
             <Stack h={"100vh"} alignItems="center" justifyContent="center" direction={["column", "row"]}>
-
                 <Card amount={5000} img={"https://cdn.shopify.com/s/files/1/1684/4603/products/MacBookPro13_Mid2012_NonRetina_Silver.png"} checkoutHandler={checkoutHandler} />
                 <Card amount={3000} img={"http://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_32c26ad194234d42b3cd9e582a21c99b"} checkoutHandler={checkoutHandler} />
-
             </Stack>
         </Box>
     )
